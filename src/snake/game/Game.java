17d6a44f65fdfde2cf2dc4ffdebf0ac1fed5741f
Jonathan E <admin@jonathane.dev>
2022-12-10 20:35:21 +0000

    Create basic object
    I created the Game, Grid and Cell classes adding some basic implementation.

--- src/snake/game/Game.java
+++ src/snake/game/Game.java
@@ -2,7 +2,15 @@ package snake.game;
 
 public class Game {
     
-    public Game(Object obj) {
-        
+    private static final int DEFAULT_GRID_SIZE = 25;
+
+    private final Grid grid;
+
+    public Game() {
+        this(DEFAULT_GRID_SIZE);
     } 
+
+    public Game(int gridSize) {
+        grid = new Grid(gridSize);
+    }
 }
---
+++ src/snake/game/Grid.java
@@ -0,0 +1,10 @@
+package snake.game;
+
+public class Grid {
+ 
+    private int size;
+
+    public Grid(int size) {
+        this.size = size;
+    }
+}
