8f862a11ae0d3c8c51f157096b1654f5368bda9b
Jonathan E <admin@jonathane.dev>
2022-12-10 21:40:24 +0000

    Create cell display
    The GridPanel has a 2D array that holds the visual representation of the cell objects.

---
+++ src/snake/gui/CellPanel.java
@@ -0,0 +1,5 @@
+package snake.gui;
+
+public class CellPanel {
+    
+}
--- src/snake/gui/GridPanel.java
+++ src/snake/gui/GridPanel.java
@@ -3,6 +3,12 @@ package snake.gui;
 import javax.swing.*;
 import java.awt.*;
 
+import snake.game.Grid;
+
 public class GridPanel extends JPanel {
     
+    private final Grid grid;
+    private final int gridSize;
+    private final CellPanel[][] cellPanel;
+
 }
