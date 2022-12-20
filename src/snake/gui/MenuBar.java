7a635e31057d87bed8e27e9956f003ed4acef666
Jonathan E <admin@jonathane.dev>
2022-12-20 19:27:35 +0000

    Place all options in a menu
    New Game and Scoreboard have been added to a dropdown menu. I have also added a help button that will lead to instructions.

--- src/snake/gui/MenuBar.java
+++ src/snake/gui/MenuBar.java
@@ -1,8 +1,8 @@
 package snake.gui;
 
-import java.awt.Dimension;
 import java.util.Optional;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -11,46 +11,64 @@ import snake.GUI;
 import snake.game.Game;
 
 public class MenuBar extends JMenuBar {
-    
-    protected final GUI gui;
-    private final JMenuItem newGame;
-    private final JMenuItem scoreBoard;
 
     public MenuBar(GUI gui) {
         super();
-        this.gui = gui;
         setVisible(true);
 
-        newGame = new JMenuItem("New Game");
-        scoreBoard = new JMenuItem("Scoreboard");
-        add(newGame);
-        add(scoreBoard);
-        newGame.addActionListener(e -> handleNew());
-        scoreBoard.addActionListener(e -> handleScoreBoard());
+        add(new OptionsMenu(gui));
     }
 
-    private void handleNew() {
-        Optional<Integer> size = selectSize();
-        if (size.isPresent()) {
-            gui.reset(size.get());
+    private class OptionsMenu extends JMenu {
+
+        private final GUI gui;
+        private final JMenuItem newGame;
+        private final JMenuItem scoreBoard;
+        private final JMenuItem help;
+    
+        OptionsMenu(GUI gui) {
+            super("☰ Menu");
+            this.gui = gui;
+            setVisible(true);
+
+            newGame = new JMenuItem("New Game");
+            scoreBoard = new JMenuItem("Scoreboard");
+            help = new JMenuItem("Help");
+            add(newGame);
+            add(scoreBoard);
+            add(help);
+            newGame.addActionListener(e -> handleNew());
+            scoreBoard.addActionListener(e -> handleScoreBoard());
+            help.addActionListener(e -> handleHelp());
         }
-    }
 
-    private Optional<Integer> selectSize() {
-        while(true) {
-            try {
-                String input = JOptionPane.showInputDialog(this, "Board Size:", Game.DEFAULT_BOARD_SIZE);
-                if (input == null) return Optional.empty();
-
-                int choice = Integer.parseInt(input);
-                if (choice > 5 && choice < 80) return Optional.of(choice);
-            } catch (NumberFormatException e) {
-                System.out.println(e);
+        private void handleNew() {
+            Optional<Integer> size = selectSize();
+            if (size.isPresent()) {
+                gui.reset(size.get());
             }
         }
-    }
+    
+        private Optional<Integer> selectSize() {
+            while(true) {
+                try {
+                    String input = JOptionPane.showInputDialog(this, "Board Size:", Game.DEFAULT_BOARD_SIZE);
+                    if (input == null) return Optional.empty();
+    
+                    int choice = Integer.parseInt(input);
+                    if (choice > 5 && choice < 80) return Optional.of(choice);
+                } catch (NumberFormatException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    
+        private void handleScoreBoard() {
+    
+        }
 
-    private void handleScoreBoard() {
+        private void handleHelp() {
 
+        }
     }
 }
